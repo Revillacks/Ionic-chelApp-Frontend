9e92755ee91b22e1bdfb5099e96519dc0b2d3a7e
alejandraxxxx <xagacia.03@gmail.com>
2024-07-17 20:52:07 -0600

    modificando login

--- src/app/features/auth/login/login.page.ts
+++ src/app/features/auth/login/login.page.ts
@@ -1,24 +1,27 @@
+
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonItem, IonIcon, IonButton, IonInput, IonGrid, IonRow, IonCol, IonImg, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
-import {add, eyeOffOutline, eyeOutline, lockClosedOutline, mailOutline} from 'ionicons/icons';
+import { add, eyeOffOutline, eyeOutline, lockClosedOutline, mailOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'login.page.html',
   styleUrls: ['login.page.scss'],
   standalone: true,
-  imports: [RouterLink,IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonImg, IonCol, IonRow, IonGrid, IonInput, IonButton, IonIcon, IonItem, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [RouterLink, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonImg, IonCol, IonRow, IonGrid, IonInput, IonButton, IonIcon, IonItem, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ReactiveFormsModule],
 })
 export class HomePage {
-  private router:Router = inject(Router);
-  formLogin! : FormGroup;
+  private router: Router = inject(Router);
+  formLogin!: FormGroup;
   showPassword: boolean = false;
-  passsInputType: string = "";
+  passsInputType: string = "password";
+  alertController: AlertController = inject(AlertController);
 
   constructor(
     private fb: FormBuilder,
@@ -34,47 +37,70 @@ export class HomePage {
       eyeOutline,
       eyeOffOutline
     });
-
-    this.passsInputType = 'password';
   }
 
-  toggleShow(){
+  toggleShow() {
     this.showPassword = !this.showPassword;
 
-    if(this.passsInputType === 'password'){
+    if (this.passsInputType === 'password') {
       this.passsInputType = 'text';
-    }else{
-    this.passsInputType = 'password';
+    } else {
+      this.passsInputType = 'password';
     }
   }
 
-  isValidControl(ctrl: string){
-    return this.formLogin.controls[ctrl] && (this.formLogin.controls[ctrl].touched || this.formLogin.controls[ctrl].dirty) ;
+  isValidControl(ctrl: string) {
+    return this.formLogin.controls[ctrl] && (this.formLogin.controls[ctrl].touched || this.formLogin.controls[ctrl].dirty);
   }
 
-  getFieldError(field: string){
-    if(!this.formLogin.controls[field]) return null;
+  getFieldError(field: string) {
+    if (!this.formLogin.controls[field]) return null;
     const errors = this.formLogin.controls[field].errors || {};
 
-    for (const key of Object.keys(errors)){
-      switch(key){
-        case'required':
-          return 'Este campo es obligatorio'
-        case'minlength':
-          return 'Minimo de caracteres requerido'
-        case'maxlength':
-          return 'Maximo de caracteres excedido'
-        case'email':
-          return 'Email no valido'
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es obligatorio';
+        case 'minlength':
+          return 'Mínimo de caracteres requerido';
+        case 'maxlength':
+          return 'Máximo de caracteres excedido';
+        case 'email':
+          return 'Email no válido';
         case 'pattern':
-          return 'Minimo una mayuscula y caracter especial'
+          return 'Mínimo una mayúscula y carácter especial';
       }
     }
     return null;
-    }
-
-  iniciar() {
-    // Lógica para iniciar sesión
   }
 
-}
+
+  async iniciar(loginExitoso: boolean) {
+    if (loginExitoso) {
+      // Muestra un cuadro de mensaje de sesión exitosa
+      const alert = await this.alertController.create({
+        header: 'Sesión Exitosa',
+        message: 'Has iniciado sesión correctamente.',
+        buttons: [{
+          text: 'OK',
+          handler: () => {
+            // Redirige a la tab2 después de cerrar la alerta
+            this.router.navigate(['/tabs/tab2']);
+          }
+        }]
+      });
+
+      await alert.present();
+    } else {
+      // Muestra un cuadro de mensaje de error
+      const alert = await this.alertController.create({
+        header: 'Error de inicio de sesión',
+        message: 'Por favor, verifica tus credenciales e inténtalo de nuevo.',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+    }
+  }
+  }
+
