1a60a4d8c5a72e38683012d1ad31352d9da2f1e0
alejandraxxxx <xagacia.03@gmail.com>
2024-08-06 09:57:01 -0600

    agregando crud para menú

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,4 +13,9 @@ export const routes: Routes = [
     path: 'register',
     loadComponent: () => import('./features/auth/register/register.page').then( m => m.RegisterPage)
   },
+  {
+    path: 'crud',
+    loadComponent: () => import('./features/auth/crud/crud.page').then( m => m.CrudPage)
+  },
+
 ];
